2f6aaf60b6247ad9e94d1bc5c8731832a661d273
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-03-29 11:22:19 +0200

    creata la searchbar

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <input type="search" />
+    <button>Cerca</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchBar",
+};
+</script>
+
+<style></style>
