85d6a657d5839d839d02cda5cd5579d3a98860f8
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-03-29 11:53:31 +0200

    implementata la chiamata all'api tramite il search

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,13 +1,31 @@
 <template>
   <div>
-    <input type="search" />
-    <button>Cerca</button>
+    <input type="search" v-model="query" />
+    <button @click="search">Cerca</button>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "SearchBar",
+  data() {
+    return {
+      query: "",
+    };
+  },
+  methods: {
+    search() {
+      axios
+        .get(
+          `https://api.themoviedb.org/3/search/movie?api_key=01af620fbe2924c05e6048caa6f5c225&language=it-IT&query=${this.query}`
+        )
+        .then((response) => {
+          console.log(response);
+        });
+    },
+  },
 };
 </script>
 
