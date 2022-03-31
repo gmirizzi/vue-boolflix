619d94e877de7be5707110b42e2535687729efc7
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-03-31 17:01:28 +0200

    popolata la select

--- src/components/FilterComponent.vue
+++ src/components/FilterComponent.vue
@@ -1,12 +1,29 @@
 <template>
   <select name="" id="">
     <option value="">Seleziona un genere</option>
+    <option v-for="genre in genres" :key="genre.id" :value="genre.name">
+      {{ genre.name }}
+    </option>
   </select>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "FilterComponent",
+  data() {
+    return {
+      genres: [],
+    };
+  },
+  beforeCreate() {
+    axios
+      .get(
+        "https://api.themoviedb.org/3/genre/movie/list?api_key=01af620fbe2924c05e6048caa6f5c225&language=it-IT"
+      )
+      .then((res) => (this.genres = res.data.genres));
+  },
 };
 </script>
 
